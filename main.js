d3b7d7be659740ba10bf3dd5cc8417e9fb8c64aa
Artjoms Ostapenko <artjom.ostapenko@gmail.com>
2017-09-27 06:28:43 +0300

    socket is failing to send

--- main.js
+++ main.js
@@ -6,8 +6,13 @@ app.controller('login', ['$scope', function($scope){
 
 
     $scope.aouthor = function(){
+        alert("kek")
         var socket = new WebSocket('wss://js-assignment.evolutiongaming.com/ws_api', 'ws');
-        socket.send(JSON.stringify($scope.credentials))
+        socket.send(JSON.stringify(                          ));
+        
+        socket.onmessage = function(event){
+            alert(event.data);
+        };
     }
     
     $scope.credentials = {
@@ -15,6 +20,7 @@ app.controller('login', ['$scope', function($scope){
         username: '',
         password: ''
     }
+    
     
 
     
