d6197ea0d4842534eaada4d31e87845c4556d70b
Artjoms Ostapenko <artjom.ostapenko@gmail.com>
2017-09-27 09:16:21 +0300

    websocket works, but  doesnt see the calue of input

--- main.js
+++ main.js
@@ -4,22 +4,35 @@
     
 app.controller('login', ['$scope', function($scope){
 
+    $scope.credentials = {
+        $type: "login",
+        username: ' ',
+        password: ''
+    }
+    
+    $scope.$type = 'login';
+    $scope.username = '';
+    $scope.password = '';
+    
 
     $scope.aouthor = function(){
-        alert("kek")
+        
+        
+        alert($scope.credentials.username, $scope.credentials.password)
         var socket = new WebSocket('wss://js-assignment.evolutiongaming.com/ws_api', 'ws');
-        socket.send(JSON.stringify(                          ));
+        
+// necessary to wait untill socket will be opened
+        socket.onopen = function(){
+            alert($scope.username, $scope.password)
+            socket.send(JSON.stringify({$scope.$type, $scope.username, $scope.password}));
+        }
         
         socket.onmessage = function(event){
             alert(event.data);
         };
     }
     
-    $scope.credentials = {
-        $type: "login",
-        username: '',
-        password: ''
-    }
+    
     
     
 
