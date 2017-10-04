c37be14bd5480e3b0802a5683d8b90eb4d6950d3
Artjoms Ostapenko <artjom.ostapenko@gmail.com>
2017-10-04 06:26:52 +0300

    working on the big handler (tableadd / table remove etc..)

--- Controllers/lobbyCtrl.js
+++ Controllers/lobbyCtrl.js
@@ -1,30 +1,25 @@
 (function(){
 app.controller('lobbyCtrl', function($scope, ws){
-    $scope.ws = ws;
-    
-    $scope.respMsg = ws.ret;
-    
+
     $scope.tableList = [{name: 'test BJ', participants: 2, id:1}];
     
     $scope.$on('response', function(event, data){
         switch (data.$type){
             case 'table_list':
-                $scope.tableList = data.tables;
-                $scope.$digest()
+                $scope.$apply(function(){$scope.tableList = data.tables});
                 break;
             case 'table_removed':
-                console.log('HURRRAAAAAAAAAAAAAAAAAY!!!!')
-        }
+                $scope.$apply(function(){
+                    $scope.tableList.forEach(function(table){
+                        if (table.id === data.id){
+                            var index = $scope.tableList.indexOf(data);
+                            $scope.tableList.splice(index);
+                            console.log($scope.tableList)
+                        }
+                    })
+                });
+        };
     });
-    
-//    $scope.$watch(function(){return ws.respMsg}, function(newVal, oldVal){
-//        console.log('$watch triggered in lobbyCtrl');
-//        
-//        
-//    }, true);
-    
-    
-    
     
     $scope.subscribe = function(type){
         var sub = {$type: type};
