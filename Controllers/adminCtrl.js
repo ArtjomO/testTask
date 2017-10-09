52c3ea036c6278dbb38d929ad7c86e9759bdbc77
Artjoms Ostapenko <artjom.ostapenko@gmail.com>
2017-10-10 01:28:44 +0300

    working version, which adds the table to the array, but also if you chenge data in admin panel, table which was added to array will ubdate too. Not good. Adding another service

--- Controllers/adminCtrl.js
+++ Controllers/adminCtrl.js
@@ -1,11 +1,39 @@
 app.controller('adminCtrl', function($scope, ws){
     
     $scope.isAdmin = 'user';
-    $scope.tableName;
-    $scope.afterId;
-    $scope.id;
     
+    $scope.table = {
+        $type: '',
+        after_id: '',
+        table: {
+            id: '',
+            name: '',
+            participants: ''
+        }
+    }
+    
+    var NewTable = function(){
+        this.$type = $scope.table.$type
+        this.after_id = $scope.table.after_id
+        this.table = $scope.table.table
+    }
+    
+    $scope.addTable = function(){
+        var table = new NewTable();
+        console.log('kek')
+        
+        switch (table.after_id) {
+            case '1':
+                
+                $scope.tableList.push(table.table)
+                console.log($scope.$parent.tableList)
+                
+                break;
+        } 
+    };
 
+    
+    
     
     $scope.$on('response', function(event, data){
         switch (data.$type) {
@@ -14,9 +42,6 @@ app.controller('adminCtrl', function($scope, ws){
                 $scope.$apply(function(){$scope.isAdmin = 'admin';})
         }
     });
-    
-
-    
     
     $scope.subscribe = function(type){
         var sub = {$type: type};
--- Controllers/lobbyCtrl.js
+++ Controllers/lobbyCtrl.js
@@ -1,6 +1,5 @@
 (function(){
 app.controller('lobbyCtrl', function($scope, ws){
-
     $scope.tableList = [{name: 'test BJ', participants: 2, id:1}];
     
     $scope.$on('response', function(event, data){
@@ -9,52 +8,25 @@ app.controller('lobbyCtrl', function($scope, ws){
         switch (data.$type){
             case 'table_list':
                 $scope.$apply(function(){$scope.tableList = data.tables});
-                console.log($scope.tableList);///////////////////////////////////////////
                 break;
-                
-                
+                  
             case 'table_removed':
                 $scope.tableList.forEach(function(table){
                     if (table.id === data.id){
                         var index = $scope.tableList.indexOf(table);
-                        console.log(index)///////////////////////////////////////////////
-                        $scope.$apply(function(){$scope.tableList.splice(index,1);})
-                        console.log($scope.tableList); ////////////////////////////  
+                        $scope.$apply(function(){$scope.tableList.splice(index,1)})
                     };
                 });
             break;
-                
-                
                 
             case 'table_added':
                 $scope.tableList.forEach(function(table){
                     if (table.id === data.after_id){
                         var index = $scope.tableList.indexOf(table) +1;
-                        console.log(index); ////////////////////////////////////////
                         $scope.$apply(function(){$scope.tableList.splice(index,0,data.table)});
-                        console.log($scope.tableList); ////////////////////////////////////////
                     };
                 });
         };
     });
-    
-//    $scope.subscribe = function(type){
-//        var sub = {$type: type};
-//        ws.send(sub);
-//
-//        if (type == 'subscribe_tables') {
-//            console.log('++Tables are subscribed..')
-//        } else if (type == 'unsubscribe_tables') {
-//            console.log('--Tables are unsubscribed..')
-//        }
-//    };
-     
-    $scope.createTable = function(type, name, participans) {
-        console.log('Creating table')
-        var table
-        
-        ws.send(table);     
-    };
-    
 });   
 })();
